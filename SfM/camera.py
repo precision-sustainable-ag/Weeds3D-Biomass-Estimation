f075b433f0806e0f6c4acf9c6ee1d34f32f43a56
Daniel Ginn <daniel.ginn@live.com.au>
2023-04-25 15:41:30 -0500

    Mesh movement for camera views working for test case

--- SfM/camera.py
+++ SfM/camera.py
@@ -1,16 +1,16 @@
 # Standard Libraries
 import open3d as o3d
 import numpy as np
+from scipy.spatial.transform import Rotation
 
 class Camera:
     PinholeCameraParameters: o3d.camera.PinholeCameraParameters
 
-    def __init__(self, translation, rotation):
-        extrinsic_np = np.zeros([4,4])
-        extrinsic_np[0:3,0:3] = rotation.as_matrix()
-        extrinsic_np[0:3,3] = translation
-        extrinsic_np[3,3] = 1
-        extrinsic = extrinsic_np
+    def __init__(self, translation: np.array, rotation: Rotation):
+        extrinsic = np.zeros([4,4])
+        extrinsic[0:3,0:3] = rotation.as_matrix()
+        extrinsic[0:3,3] = translation
+        extrinsic[3,3] = 1
         intrinsic = o3d.camera.PinholeCameraIntrinsic(width=1920, height=1080, fx=997.5123, fy=999.109885, cx=953.45, cy=543.51542)
         self.PinholeCameraParameters = o3d.camera.PinholeCameraParameters()
         self.PinholeCameraParameters.intrinsic = intrinsic
--- tests/test_pointcloud.py
+++ tests/test_pointcloud.py
@@ -1,26 +1,33 @@
 from unittest import TestCase
 from SfM.pointcloud import PointCloud
-from SfM.pointcloud import Camera
+from SfM.camera import Camera
 import open3d as o3d
 from scipy.spatial.transform import Rotation as R
 import numpy as np
 
 class TestPointCloud(TestCase):
     def setUp(self):
+
         self.pcloud = PointCloud("./pointcloudtest.txt.ply")
         self.pcloud.mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd=self.pcloud.data,
                                                                                            radii=o3d.utility.DoubleVector(
                                                                                                [0.8]))
-        camera1 = Camera(translation=np.array([-3, 1.5, 0.5]), rotation=R.from_euler('zyx', [0, 0, 0], degrees=True))
-        camera2 = Camera(translation=np.array([1, -3, 0.5]), rotation=R.from_euler('zyx', [90, 0, 0], degrees=True))
-        camera3 = Camera(translation=np.array([1, 1.5, 4]), rotation=R.from_euler('zyx', [0, 90, 0], degrees=True))
+        #self.pcloud.rotate(np.pi / 2 * np.array([1, 0, 0]))
+        Rot = self.pcloud.mesh.get_rotation_matrix_from_xyz((np.pi / 2, 0, 0))
+        self.pcloud.mesh.rotate(Rot, center=(0, 0, 0))
+
+        camera1 = Camera(translation=np.array([-1, 0.5, 3]), rotation=R.from_euler('zyx', [0, 0, 0], degrees=True)) # 2x1 viewing towards +y, up +z, p @ top left
+        camera2 = Camera(translation=np.array([-1.5, -1, 4]), rotation=R.from_euler('zyx', [90, 90, 0], degrees=True)) # 3x2 viewing towards -z, up -x, p @ top left
+        camera3 = Camera(translation=np.array([1.5, 0.5, 3]), rotation=R.from_euler('zyx', [0, -90, 0], degrees=True)) # 3x1 viewing towards +x, up +z, p @ top right
         self.pcloud.cameras.append(camera1)
         self.pcloud.cameras.append(camera2)
         self.pcloud.cameras.append(camera3)
-        # vis = o3d.visualization.Visualizer()
-        # vis.create_window()
-        # vis.add_geometry(self.pcloud.mesh)
-        # vis.run()
+        vis = o3d.visualization.Visualizer()
+        vis.create_window(width=1920, height=1080)
+        vis.add_geometry(self.pcloud.mesh)
+        ctr = vis.get_view_control()
+        ctr.convert_from_pinhole_camera_parameters(camera3.PinholeCameraParameters, allow_arbitrary=True)
+        vis.run()
 
     def test_mesh(self):
         self.assertEqual(1, 1)
