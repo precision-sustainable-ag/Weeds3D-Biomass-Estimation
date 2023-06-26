1e97a850e4938831fc7b1ba69faac2937aef020b
Daniel <daniel.ginn@live.com.au>
2023-06-26 15:40:26 -0500

    Point cloud test distortion works

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,6 @@
+from SfM.pointcloud import PointCloud
+from SfM.camera import Camera
+import open3d as o3d
 from scipy.spatial.transform import Rotation as R
 import numpy as np
 
@@ -15,29 +18,30 @@ print(cam_vec)
 cam_pos = -1 * Rot.as_matrix().T @ t
 
 print(cam_pos)
-
-P_o2p_wrt_w = np.array([[0, 0, 0],
-                  [0, 3, 0],
-                  [2, 3, 0],
-                  [2, 0, 0],
-                  [0, 0, 1],
-                  [0, 3, 1],
-                  [2, 3, 1],
-                  [2, 0, 1]])
-
-P_c2p_wrt_c = np.zeros([8,3])
-P_c2p_wrt_c_znorm = np.zeros([8,3])
-P_c2p_wrt_c_pix = np.zeros([8,3])
+pcloud = PointCloud("./pointcloudtest.txt.ply")
+P_o2p_wrt_w = pcloud.get_points()
+num_points = pcloud.get_num_points()
+P_c2p_wrt_c = np.zeros([num_points,3])
+P_c2p_wrt_c_znorm = np.zeros([num_points,3])
+P_c2p_wrt_c_pix = np.zeros([num_points,3])
 
 f = 1000
 k1 = 0 # -0.126
 k2 = 0 # 0.004
 
-for i in range(8):
+for i in range(num_points):
     X = P_o2p_wrt_w[i, :]
     P_c2p_wrt_c[i, :] = Rot.as_matrix() @ X.T + t
     P_c2p_wrt_c_znorm[i, :] = -P_c2p_wrt_c[i, :] / P_c2p_wrt_c[i, 2]  # Perspective Division by -z axis
     P_c2p_wrt_c_pix[i, :] = f * undistort(P_c2p_wrt_c_znorm[i, :], k1, k2) * P_c2p_wrt_c_znorm[i, :]
 
-print(P_c2p_wrt_c_pix)
+pcloud.set_points(P_c2p_wrt_c_pix)
+
+vis = o3d.visualization.Visualizer()
+vis.create_window(width=3840, height=2160)
+vis.add_geometry(pcloud.data)
+ctr = vis.get_view_control()
+camera = Camera(translation=np.array([0, 0, 0]), rotation=R.from_euler('zyx', [0, 0, 180], degrees=True))
+ctr.convert_from_pinhole_camera_parameters(camera.PinholeCameraParameters, allow_arbitrary=True)
+vis.run()
 
